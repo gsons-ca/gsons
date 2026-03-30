e68719ba0fba8d9cbbd07fc8b88f9d3bf7bafc43
Your Name <you@example.com>
2026-03-30 13:58:29 -0700

    about page added

---
+++ app/about/page.tsx
@@ -0,0 +1,24 @@
+import Header from "../components/template-parts/Header";
+import Hero from "../components/template-parts/Hero";
+import WhyChooseUs from "../components/template-parts/WhyChooseUs";
+import Vision from "../components/template-parts/Vision";
+import Solutions from "../components/template-parts/Solutions";
+import Statistics from "../components/template-parts/Statistics";
+import Services from "../components/template-parts/Services";
+import Testimonials from "../components/template-parts/Testimonials";
+import CTA from "../components/template-parts/CTA";
+import Footer from "../components/template-parts/Footer";
+
+export default function About() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1">
+       
+        <Vision />
+       
+      </main>
+      <Footer />
+    </div>
+  );
+}
